a01753b723dce9994ad60e1486f5299b43944098
Lukada-taiya <lukmanadam43@gmail.com>
2024-06-06 17:22:58 +0000

    Changed frontend bundler to vite. Development will be started from scratch

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,19 +1,35 @@
-import './App.css';  
-import AppRoutes from './Components/Layouts/AppRoutes';
-import { Fragment } from 'react/jsx-runtime';
-import { BrowserRouter as Router} from 'react-router-dom'; 
-import { SkeletonTheme } from 'react-loading-skeleton';
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
 
 function App() {
+  const [count, setCount] = useState(0)
+
   return (
-    <Fragment> 
-      {/* <SkeletonTheme baseColor="#202020" highlightColor="#444"> */}
-        <Router>
-          <AppRoutes/>
-        </Router> 
-        {/* </SkeletonTheme> */}
-    </Fragment>
-  );
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
 }
 
-export default App;
+export default App
