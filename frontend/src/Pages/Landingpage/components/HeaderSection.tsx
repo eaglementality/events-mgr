5efcf4356d06a5c73c60df9c8d21d3a1f5e224ab
eaglementality <jesseowusuyeboah99@gmail.com>
2024-06-13 21:15:15 +0000

    Refactored code

--- frontend/src/Pages/Landingpage/components/HeaderSection.tsx
+++ frontend/src/Pages/Landingpage/components/HeaderSection.tsx
@@ -1,24 +1,68 @@
-import logo from "../../../assets/Event_Bliss-removebg-preview.png";
-import cheer from "../../../assets/cheering.jpg";
-
+import { Carousel } from "flowbite-react";
+import { RiCloseLargeFill, RiMenu3Fill } from "react-icons/ri";
+import { CustomDrawer } from "../../../templates/CustomDrawer";
+import noImage from '../../../assets/No Image.jpg';
+import { useState } from "react";
 export const HeaderSection = () => {
+  const [isOpen, setIsOpen] = useState(false);
   return (
-    <main className="w-full ">
-      <img src={cheer} alt="logo" className="w-screen h-screen" />
-      <header className="absolute z-50 inset-0 top-0 bg-black opacity-80">
-        <nav className="flex justify-between w-full px-48 py-6">
-          <img src={logo} alt="logo" className="w-[10%] -mt-4 " />
-          <div className="flex space-x-12 mt-10">
-            <ul className="text-white flex space-x-10">
-              <li>{`Schedule`}</li>
-              <li>{`Speakers`}</li>
-              <li>{`Ticket`}</li>
-              <li>{`Contact`}</li>
-            </ul>
-            <button className=" w-[101px] h-[41px] -mt-[8.5px] text-white border-[1.5px] border-white rounded-l-full rounded-r-full">{`Login`}</button>
+    <>
+      <CustomDrawer
+        isOpen={isOpen}
+        DrawerStyle="w-[12rem]"
+        titleIcon={<span className="hidden"></span>}
+      />
+
+      <main className="w-full h-full">
+        <nav className="bg-black flex justify-between px-48 py-6 w-full mobile:py-2 mobile:px-2 desktop:px-48 desktop:py-6 ">
+          <h1 className="text-white px-8 py-2 font-extrabold text-xl mobile:text-base mobile:px-4">{`Event Bliss`}</h1>
+          <div className="flex space-x-12 mobile:hidden desktop:flex laptop:flex ">
+            <div className="text-white flex space-x-10 px-8 py-2  ">
+              <span>{`Schedule`}</span>
+              <span>{`Speakers`}</span>
+              <span>{`Ticket`}</span>
+              <span>{`Contact`}</span>
+            </div>
+            <button className="text-white border-[0.5px] border-white rounded-3xl px-8 py-2">{`Log in`}</button>
           </div>
+          {isOpen ? (
+            <RiCloseLargeFill
+              className="text-white mx-4 mt-3 desktop:hidden laptop:hidden"
+              onClick={() => {
+                setIsOpen(true);
+              }}
+            />
+          ) : (
+            <RiMenu3Fill
+              className="text-white mx-4 mt-3 desktop:hidden laptop:hidden"
+              onClick={() => {
+                setIsOpen(true);
+              }}
+            />
+          )}
         </nav>
-      </header>
-    </main>
+        <section className="h-full w-full">
+          <Carousel className="mobile:h-64 desktop:h-[720px] tablet:h-[720px] laptop:h-[720px]">
+            <div className="flex h-full items-center justify-center bg-gray-400 dark:bg-gray-700 dark:text-white space-x-8">
+              <img src={noImage} alt="" className="h-[20rem] w-[20rem] rounded-lg"/>
+              <div className="w-[30rem] ">
+                <h3 className="text-2xl font-bold mb-6">{`SBS MTV The Kpop Show Ticket  Package`}</h3>
+                <p className="mb-8">{`Look no further! Our SBS The Show tickets are the simplest way for you to experience a live Kpop recording.`}</p>
+                <div className="flex space-x-2">
+                  <button className="py-4 px-10  bg-black text-white rounded-xl">{`Get Ticket`}</button>
+                  <button className="py-4 px-10  border border-white rounded-xl">{`Learn More`}</button>
+                </div>
+              </div>
+            </div>
+            {/* <div className="flex h-full items-center justify-center bg-gray-400 dark:bg-gray-700 dark:text-white">
+              Slide 2
+            </div>
+            <div className="flex h-full items-center justify-center bg-gray-400 dark:bg-gray-700 dark:text-white">
+              Slide 3
+            </div> */}
+          </Carousel>
+        </section>
+      </main>
+    </>
   );
 };
