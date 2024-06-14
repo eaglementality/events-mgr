17fd0e7ac1c00d35e4dbcbdf959a3fd6bd7e52bd
eaglementality <jesseowusuyeboah99@gmail.com>
2024-06-14 18:20:51 +0000

    Add:  upcoming event page

--- frontend/src/Pages/Landingpage/landingPage.tsx
+++ frontend/src/Pages/Landingpage/landingPage.tsx
@@ -1,9 +1,11 @@
 import { HeaderSection } from "./components/HeaderSection";
+import { UpComingEvent } from "./components/UpComingEvent";
 
 export const LandingPage = () => {
   return (
     <main className="w-full h-full">
-      <HeaderSection />
+      <HeaderSection/>
+      <UpComingEvent/>
     </main>
   );
 };
